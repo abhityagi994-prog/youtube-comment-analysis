b9a27649aac8db31e8b94ae6a38091310ebf2542
Abhi Tyagi <abhi.tyagi994@gmail.com>
2026-09-10 00:01:36 +0100

    Initial project setup with model experimentation and analysis

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end-to-end MLOps project for analyzing sentiment in YouTube comments.',
+    author='abhi',
+    license='MIT',
+)
